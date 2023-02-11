b3105eba632c2383106af046d14cb1393fcb056e
kubagrzelak <jakubgrzelak1@gmail.com>
2023-02-11 22:35:40 +0100

    Added response status code for downloading the model.

--- src/module/download_model.py
+++ src/module/download_model.py
@@ -20,10 +20,13 @@ def download_model():
             resp = requests.get(model_url, allow_redirects=True)
 
             # save the model
-            log.info(f"Saving the model inside Docker container in: model/{getenv('MODEL_FILENAME')} ...")
-            open(f"model/{getenv('MODEL_FILENAME')}", 'wb').write(resp.content)
+            if resp.status_code == 200:
+                log.info(f"Saving the model inside Docker container in: model/{getenv('MODEL_FILENAME')} ...")
+                open(f"model/{getenv('MODEL_FILENAME')}", 'wb').write(resp.content)
 
-            log.info("Successfully downloaded the model.")
+                log.info("Successfully downloaded the model.")
+            else:
+                log.error(f"Could not download the model. {resp.status_code} - {resp.reason}")
 
         except Exception as e:
             log.error(f"Exception when downloading the model: {e}")
